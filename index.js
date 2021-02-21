7109813decdb4a227009a4d2646d57b6ccbd43e8
Jose Maria <familia@DESKTOP-RHSGVJV.localdomain>
2021-02-21 17:41:08 +0100

    Aguujeros zombi mas juntos

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ for (const boton of botones) {
     boton.addEventListener('click', evento => {
         switch(evento.target.id){
             case 'autonomo':
-                location.assign(`${location.href+'/autonomo.html'}`)
+                location.assign(`${location.href+'autonomo.html'}`)
                 break
             case 'calculadora':
-                location.assign(`${location.href+'/calculadora.html'}`)
+                location.assign(`${location.href+'calculadora.html'}`)
                 break
             case 'divisas':
-                location.assign(`${location.href+'/divisas.html'}`)
+                location.assign(`${location.href+'divisas.html'}`)
                 break
             case 'zombies':
-                location.assign(`${location.href+'/zombies.html'}`)
+                location.assign(`${location.href+'zombies.html'}`)
                 break
         }
     })
